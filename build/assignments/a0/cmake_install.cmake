5f244eab2c94824f0a7ffa354dcc254721ac5b75
Joshua Pfefferkorn <77121865+jpfeff@users.noreply.github.com>
2023-03-08 13:45:45 -0500

    changed gitignore

--- build/assignments/a0/cmake_install.cmake
+++ build/assignments/a0/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/kevin/Desktop/Dartmouth/2022-23/23W/CS77/Final Project/cpk_cs77final/assignments/a0
+# Install script for directory: /Users/joshpfefferkorn/Desktop/COSC 77/Project/cpk_cs77final/assignments/a0
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
